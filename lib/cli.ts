5eedf301736c07dc5cd9d5433bd841e9d61dfa9b
Joe Buza <jbuza@netflix.com>
2024-02-16 02:53:43 -0800

    add linter and formatter

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,27 +1,30 @@
 #!/usr/bin/env node
-import * as fs from 'fs/promises';
+import * as fs from "fs/promises";
 import { transformMexcToCoinTracker } from "./convertMexcToCointracker";
 
 async function main(inputFilePath: string, outputDirectory: string) {
   try {
     // Check if the output directory exists, create if it doesn't
-    const dirExists = await fs.access(outputDirectory).then(() => true).catch(() => false);
+    const dirExists = await fs
+      .access(outputDirectory)
+      .then(() => true)
+      .catch(() => false);
     if (!dirExists) {
       await fs.mkdir(outputDirectory, { recursive: true });
       console.log(`Output directory created at ${outputDirectory}`);
     }
 
     await transformMexcToCoinTracker(inputFilePath, outputDirectory);
-    console.log('Transformation completed successfully.');
+    console.log("Transformation completed successfully.");
   } catch (error) {
-    console.error('An error occurred:', error);
+    console.error("An error occurred:", error);
     process.exit(1);
   }
 }
 
 const args = process.argv.slice(2);
 if (args.length !== 2) {
-  console.log('Usage: bun index.ts <inputFilePath> <outputDirectory>');
+  console.log("Usage: bun index.ts <inputFilePath> <outputDirectory>");
   process.exit(1);
 }
 const [inputFilePath, outputDirectory] = args;
---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+// vite.config.ts
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
+// https://vitejs.dev/guide/build.html#library-mode
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/index.ts"),
+      name: "mexc-to-cointracker",
+      fileName: "mexc-to-cointracker",
+    },
+  },
+  plugins: [dts()],
+});
